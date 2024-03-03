b09ccf6474bc09fdf1e1fa1c6cc3add4711bd200
Ricky9702 <khuong93qt@gmail.com>
2024-03-03 17:50:53 +0700

    Add notification module, create schedule,...

--- src/main/java/com/med/service/MedicationScheduleService.java
+++ src/main/java/com/med/service/MedicationScheduleService.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -52,17 +53,24 @@ public class MedicationScheduleService {
                 schedule.setMedicineUnit(payload.getMedicineUnit());
                 schedule.setIsActive(payload.getIsActive());
 
+                repository.save(schedule);
+
                 // Get all existing items in the database
                 List<ScheduleTimeDTO> existingScheduleTimes = scheduleTimeRepository.getScheduleTimeByMedicationScheduleId(payload.getId());
 
-                // Delete items not present in the updated list
+                // Get All item Ids in the updated list
+                Set<Integer> updatedTimeIds = payload.getScheduleTimes().stream()
+                        .map(ScheduleTimeDTO::getId)
+                        .collect(Collectors.toSet());
+
+                // Delete items not present in the updated list using the repository
                 existingScheduleTimes.stream()
-                        .filter(existed -> !payload.getScheduleTimes().contains(existed))
-                        .forEach(dto -> scheduleTimeRepository.deleteById(dto.getId()));
+                        .filter(scheduleTimeDTO -> !updatedTimeIds.contains(scheduleTimeDTO.getId()))
+                        .forEach(scheduleTimeDTO -> scheduleTimeRepository.deleteById(scheduleTimeDTO.getId()));
 
                 // Create new items for items with id as null
                 payload.getScheduleTimes().stream()
-                        .filter(created -> created.getId() == null)
+                        .filter(created -> created.getId() == 0)
                         .forEach(created -> {
                             ScheduleTime scheduleTime = new ScheduleTime(
                                     0,
@@ -85,21 +93,20 @@ public class MedicationScheduleService {
                         payload.getMedicineUnit(),
                         payload.getIsActive() != null ? payload.getIsActive() : true
                 );
+
+                // Save or update MedicationSchedule
+                MedicationSchedule savedSchedule = repository.save(schedule);
+
+                // Loop through scheduleTimes list and create MedicationScheduleTime in the database
+                payload.getScheduleTimes().forEach(scheduleTime -> {
+                    ScheduleTime object = new ScheduleTime(
+                            0,
+                            scheduleTime.getTime(),
+                            scheduleTime.getQuantity(),
+                            savedSchedule);
+                    scheduleTimeRepository.save(object);
+                });
             }
-
-            // Save or update MedicationSchedule
-            MedicationSchedule savedSchedule = repository.save(schedule);
-
-            // Loop through scheduleTimes list and create MedicationScheduleTime in the database
-            payload.getScheduleTimes().forEach(scheduleTime -> {
-                ScheduleTime object = new ScheduleTime(
-                        0,
-                        scheduleTime.getTime(),
-                        scheduleTime.getQuantity(),
-                        savedSchedule);
-                scheduleTimeRepository.save(object);
-            });
-
             return ResponseEntity.status(HttpStatus.CREATED).body("MedicationSchedule created or updated successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
