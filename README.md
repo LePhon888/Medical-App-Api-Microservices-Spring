a182f86c70ed7140160f3a6b42f32f5d06044600
Ricky9702 <khuong93qt@gmail.com>
2024-03-04 20:26:06 +0700

    rebase into microservices

--- README.md
+++
@@ -1,6 +0,0 @@
-# MedicalAppBackEnd
-MedicalAppBackEnd
-
-1. Clone project
-2. Open project in Intellij IDEA
-3. Run
--- src/main/java/com/med/repository/HourRepository.java
+++
@@ -1,7 +0,0 @@
-package com.med.repository;
-
-import com.med.model.Hour;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HourRepository extends JpaRepository<Hour, Integer> {
-}
