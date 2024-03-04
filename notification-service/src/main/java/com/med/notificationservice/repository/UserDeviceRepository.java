4bf375f9ad0725d68b8d5db28a3ecaf88e504724
Ricky9702 <khuong93qt@gmail.com>
2024-03-04 21:06:18 +0700

    fixed merge project

---
+++ notification-service/src/main/java/com/med/notificationservice/repository/UserDeviceRepository.java
@@ -0,0 +1,8 @@
+package com.med.notificationservice.repository;
+
+import com.med.notificationservice.model.UserDevice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDeviceRepository extends JpaRepository<UserDevice, Integer> {
+    UserDevice findUserDeviceByUserId(Integer userId);
+}
