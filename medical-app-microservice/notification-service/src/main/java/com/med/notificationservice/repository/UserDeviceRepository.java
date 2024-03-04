10dd8a1750448f1187618241fcfd077daa85ff3c
Ricky9702 <khuong93qt@gmail.com>
2024-03-04 21:07:24 +0700

    fixed merge project

--- medical-app-microservice/notification-service/src/main/java/com/med/notificationservice/repository/UserDeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.med.notificationservice.repository;
-
-import com.med.notificationservice.model.UserDevice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDeviceRepository extends JpaRepository<UserDevice, Integer> {
-    UserDevice findUserDeviceByUserId(Integer userId);
-}
