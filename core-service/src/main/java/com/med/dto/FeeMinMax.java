c914800e802919a8db6a63723b710f2a89dbea44
Ricky9702 <khuong93qt@gmail.com>
2024-04-10 20:59:17 +0700

    Add filter for doctor list, attempt to alter application name, notification icon, set auto reschedule for medication reminder

---
+++ core-service/src/main/java/com/med/dto/FeeMinMax.java
@@ -0,0 +1,17 @@
+package com.med.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeeMinMax {
+    private BigDecimal minValue;
+    private BigDecimal maxValue;
+}
