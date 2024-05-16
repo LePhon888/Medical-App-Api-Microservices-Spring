829f6b7bcdc83cb40d9e25c575c8503253f982f2
Le Phon <lephon888@gmail.com>
2024-05-16 18:03:43 +0700

    add swagger api documents

---
+++ medication-service/src/main/java/com/medical/medicationservice/config/OpenApiConfigs.java
@@ -0,0 +1,40 @@
+package com.medical.medicationservice.config;
+
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenApiConfigs {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String url = "http://localhost:8080";
+        final String serviceTitle = "Medication Service";
+        final String serviceVersion = "1.0.0";
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .servers(List.of(new Server().url(url)))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
+                .info(new Info().title(serviceTitle).version(serviceVersion));
+    }
+}
